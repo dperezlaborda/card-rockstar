8af4807d3ab511cc61ce510447de6846fe33884f
dolores <dperezlaborda@gmail.com>
2020-03-11 13:30:22 -0300

    se agregan botones con estilo

--- src/Sections/Header/Header.js
+++ src/Sections/Header/Header.js
@@ -7,15 +7,17 @@ const Header = () =>{
             <div className="container-text">
             <h1>I'll be a rock 'n' rollin' bitch for you</h1>
                 <div className="right">
-                    <div className="rectangle"></div>
-                    <h2>moonage dream</h2>
-                    <p>- david bowie</p>
+                    <div className="rectangle">
+                        <h2>moonage dream</h2>
+                        <p>- david bowie</p>
+                    </div>
                 </div>
             </div>
-            <nav>
-                <a href="">Home</a>
-                <a href="">About</a>
-            </nav>
+            <div class="container-bttn"> 
+                <a href="#" class="btn btn-sm animated-button thar-two">home</a>
+                <a href="#" class="btn btn-sm animated-button thar-two">about</a>
+                <a href="#" class="btn btn-sm animated-button thar-two">news</a>
+            </div>
         </header>
     )
 }
