166577bc91df736eb9faa8e814be64046a48c4b5
dolores <dperezlaborda@gmail.com>
2020-03-13 14:40:10 -0300

    footer terminado, falta ordenar alfabeticamente el codigo y detalles

--- src/Sections/Footer/Footer.js
+++ src/Sections/Footer/Footer.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import './Footer.scss';
 
 const Footer = () =>{
     return(
-        <footer>
-            
+        <footer className="container-ftr">
+            <ul className="no-list container-links">
+                <li>
+                    <a className="logo" href=" https://www.linkedin.com/in/dolores-perez-laborda/" aria-label="Link to LinkedIn">
+                        <i class="fa fa-linkedin"  aria-hidden="true"></i>
+                    </a>
+                </li>
+                <li>
+                    <a className="logo" href="https://github.com/dperezlaborda" aria-label="Link to GitHub">
+                        <i class="fa fa-github" aria-hidden="true"></i>
+                    </a>
+                </li>
+                <li>
+                    <a className="logo" href="https://www.instagram.com/loli.perezlaborda/" aria-label="Link to Instagram">
+                        <i class="fa fa-instagram" aria-hidden="true"></i>
+                    </a>
+                </li>
+            </ul>
+            <div className="box-ftr">
+                <p>© 2020 Dolores Perez Laborda. All rights reserved.</p>
+            </div>
         </footer>
     )
 }
